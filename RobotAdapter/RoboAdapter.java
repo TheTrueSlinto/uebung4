fa16386e84f2bd64d6af88638ad704f93ed3acec
TheTrueSlinto <TheTrueSlinto@users.noreply.github.com>
2017-11-23 21:21:18 +0100

    Returnmethoden gefixed

--- RobotAdapter/RoboAdapter.java
+++ RobotAdapter/RoboAdapter.java
@@ -8,21 +8,21 @@ public class RoboAdapter implements Spieler {
     
     public int geheNachLinks(int a) {
         roboter.rolleNach(roboter.getX()-a, roboter.getY());
-        return (int)roboter.getX()-a;
+        return (int)roboter.getX();
     }
     
     public int geheNachRechts(int a) {
         roboter.rolleNach(roboter.getX()+a, roboter.getY());
-        return (int)roboter.getX()+a;
+        return (int)roboter.getX();
     }
     
     public int geheNachOben(int a) {
         roboter.rolleNach(roboter.getX(), roboter.getY()+a);
-        return (int)roboter.getY()+a;
+        return (int)roboter.getY();
     }
     
     public int geheNachUnten(int a) {
         roboter.rolleNach(roboter.getX(), roboter.getY()-a);
-        return (int)roboter.getY()-a;
+        return (int)roboter.getY();
     }
 }
